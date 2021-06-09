a69cbfc06a037142e7d4f86a2575ee48fd2ae4e0
Jason <bilafish@gmail.com>
2021-06-09 20:40:58 +0800

    chore: add proxima nova bold font

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,24 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/proxima-nova-soft.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <title>Mahjong with Friends</title>
+        <meta
+          name="description"
+          content="Stay home, stay safe. Play mahjong with friends from anywhere in the world."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
